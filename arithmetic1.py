f0abee389d203462a4fc45f82185a131ed79e278
Hackbright Student <no-reply@hackbrightacademy.com>
2016-09-29 13:04:55 -0700

    Fixed multiple + - * to take multiple inputs

--- arithmetic1.py
+++ arithmetic1.py
@@ -1,19 +1,21 @@
 
-def add(*nums):
-    sum_all = nums[0]
-    for num in nums[1:]:
-        sum_all += num
+def add(values):
+    sum_all = 0
+    for nums in values:
+        sum_all += int(nums)
     return sum_all
 
-def subtract(*nums):
-    difference = nums[0]
-    for num in nums[1:]:
-        difference -= num
-    return difference
+def subtract(values):
+    sum_all = values[0]
+    for nums in values[1:]:
+        sum_all -= int(nums)
+    return sum_all
 
-def multiply(num1, num2):
-    return num1 * num2
-
+def multiply(values):
+    sum_all = 1
+    for nums in values:
+        sum_all *= int(nums)
+    return sum_all
 
 def divide(num1, num2):
     # Need to turn at least argument to float for division to
--- calculator.py
+++ calculator.py
@@ -5,7 +5,7 @@ Using our arithmetic.py file from Exercise02, create the
 calculator program yourself in this file.
 """
 
-from arithmetic import *
+from arithmetic1 import *
 
 
 # Your code goes here
@@ -13,36 +13,47 @@ def calc2():
     """Calculates numbers and operations in a specific format"""
 
     command = raw_input("> ")
+    booger = "can't read, try +, -, *, /, square, cube, pow, or mod."
     while command != "q":
         tokens = command.split(" ")
         operand = tokens[0]
-        if len(tokens) == 2:
-            num1 = int(tokens[1])
-            if operand == "square":
-                print square(num1)
-            elif operand == "cube":
-                print cube(num1)
-            else:
-                print "can't read, try +, -, *, /, square, cube, pow, or mod."
-        elif len(tokens) == 3:
-            num1 = int(tokens[1])
-            num2 = int(tokens[2])
-            if operand == "+":
-                print add(num1, num2)
-            elif operand == "-":
-                print subtract(num1, num2)
-            elif operand == "*":
-                print multiply(num1, num2)
-            elif operand == "/":
-                print divide(num1, num2)
-            elif operand == "pow":
-                print power(num1, num2)
-            elif operand == "mod":
-                print mod(num1, num2)
-            else:
-                print "can't read, try +, -, *, /, square, cube, pow, or mod."
+        symbol = ["+","-","*","/","square","cube","pow","mod"]
+        
+        if operand in symbol:
+            if operand == "square" or operand == "cube":
+                num1 = int(tokens[1])
+                if operand == "square":
+                    print square(num1)
+                elif operand == "cube":
+                    print cube(num1)
+                else:
+                    print booger
+
+            elif operand == "/" or operand == "pow" or operand == "mod":
+                num1 = int(tokens[1])
+                num2 = int(tokens[2])
+                if operand == "/":
+                    print divide(num1, num2)
+                elif operand == "pow":
+                    print power(num1, num2)
+                elif operand == "mod":
+                    print mod(num1, num2)
+                else:
+                    print booger
+
+            elif operand == "+" or operand == "-" or operand == "*":
+                values = tokens[1:]
+                if operand == "+":
+                    print add(values)
+                elif operand == "-":
+                    print subtract(values)
+                elif operand == "*":
+                    print multiply(values)
+                else:
+                    print booger
+
         else:
-            print "can't read, try +, -, *, /, square, cube, pow, or mod."
+            print booger
         command = raw_input("> ")
 
 calc2()
